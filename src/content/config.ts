08cfb2eded18ecfedf59e2ef9aef68f283238481
BoJoNVi <bonjomontes@gmail.com>
2023-03-21 17:44:04 +0800

    content(products): added some data form the source

---
+++ src/content/config.ts
@@ -0,0 +1,12 @@
+import { defineCollection, z } from "astro:content";
+
+const productSchema = defineCollection({
+  schema: z.object({
+    name: z.string(),
+    isFeatured: z.boolean(),
+  }),
+});
+
+export const collections = {
+  products: productSchema,
+};

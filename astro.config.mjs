47e9559c50a4c5c467195b0cabc466a90880208d
BoJoNVi <bonjomontes@gmail.com>
2023-03-21 16:14:52 +0800

    build(astro): added site property of the website

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 
 export default defineConfig({
+  site: "https://www.asiseal.com",
   integrations: [tailwind(), sitemap(), svelte()],
 });

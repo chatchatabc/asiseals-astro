59e56cac5890514d93fa22a7519dc75cbaead0bc
BoJoNVi <bonjomontes@gmail.com>
2023-03-21 16:49:46 +0800

    build: added mdx package to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
+import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   site: "https://www.asiseal.com",
-  integrations: [tailwind(), sitemap(), svelte()],
+  integrations: [tailwind(), sitemap(), svelte(), mdx()],
 });

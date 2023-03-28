a921c1d1534480f8903733d0daec0d2b8b4ca2ac
BoJoNVi <bonjomontes@gmail.com>
2023-03-28 13:43:57 +0800

    feat: implemented the factory tour page

---
+++ src/components/about-us/FactoryTour.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+  import factoryTourJson from "@data/factory-tour.json";
+
+  let selected: string | null = null;
+
+  $: console.log(selected);
+
+  $: filteredList = factoryTourJson.contents.filter((item) => {
+    if (selected === null) {
+      return true;
+    } else if (selected) {
+      return item.type === selected;
+    }
+  });
+
+  function handleSelect(value: string) {
+    if (selected === value) {
+      selected = null;
+    } else {
+      selected = value;
+    }
+  }
+</script>
+
+<section class="max-w-7xl mx-auto px-4 lg:px-8">
+  <header class="flex space-x-8 justify-center py-8">
+    <button
+      on:click={() => handleSelect("test-center")}
+      class={`px-8 py-2 text-lg ${
+        selected !== "test-center"
+          ? "bg-primary text-onPrimary"
+          : "bg-white border border-primary text-primary"
+      }`}
+    >
+      Test Center &gt;
+    </button>
+    <button
+      on:click={() => handleSelect("production-equipment")}
+      class={`px-8 py-2 text-lg ${
+        selected !== "production-equipment"
+          ? "bg-primary text-onPrimary"
+          : "bg-white border border-primary text-primary"
+      }`}
+    >
+      Production Equipments &gt;
+    </button>
+  </header>
+
+  <section class="pb-16">
+    <ul class="flex flex-wrap">
+      {#each filteredList as factory (factory.imageUrl)}
+        <li class="p-2 w-1/2 md:w-1/3 lg:w-1/4">
+          <figure class="relative">
+            <div>
+              <img src={factory.imageUrl} alt={factory.name} />
+            </div>
+            <figcaption
+              class="absolute bg-black bg-opacity-30 text-onPrimary py-2 text-xl text-center w-full bottom-0"
+            >
+              <p>{factory.name}</p>
+            </figcaption>
+          </figure>
+        </li>
+      {/each}
+    </ul>
+  </section>
+</section>

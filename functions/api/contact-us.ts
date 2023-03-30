5de59aab4e5cf3355649c6cbff4acaa7ddb1eef2
BoJoNVi <bonjomontes@gmail.com>
2023-03-30 14:39:03 +0800

    feat: implemented POST api for contact us form

--- functions/api/contact-us.ts
+++ functions/api/contact-us.ts
@@ -3,18 +3,81 @@ import { ContactUsFormSchema } from "../schema/ContactUsSchema";
 export async function onRequest(context) {
   const { request } = context;
 
-  // Handle POST requests
   if (request.method === "POST") {
     const data = await request.json();
+    const date = new Date();
+    const timeFormatted = new Intl.DateTimeFormat("en-US", {
+      timeStyle: "short",
+    }).format(date);
+    const dateFormatted = new Intl.DateTimeFormat("en-US", {
+      dateStyle: "short",
+    }).format(date);
+
     try {
+      // Validate the data
       ContactUsFormSchema.parse(data);
-      if (!data.companyName) data.companyName = "Anonymous";
 
-      
+      // Set default values
+      if (!data.companyName) data.companyName = "Anonymous Company";
 
-      return new Response(JSON.stringify(data), { status: 200 });
+      // Create email body
+      const body = {
+        personalizations: [
+          {
+            to: [
+              {
+                email: "lucious.greenfelder97@ethereal.email",
+                name: "Lucious Greenfelder",
+              },
+            ],
+          },
+        ],
+        from: {
+          email: "contact_us_form@asiseal.com",
+          name: "Asiseal Contact Us Form",
+        },
+        subject: "Contact Us Form Submission - " + dateFormatted,
+        content: [
+          {
+            type: "text/plain",
+            value: `
+            Contact Us Form Submission - ${dateFormatted} ${timeFormatted}
+            Name: ${data.name}
+            Email: ${data.email}
+            Company Name: ${data.companyName}
+            Telephone: ${data.telephone}
+            Message: ${data.message}
+          `,
+          },
+          {
+            type: "text/html",
+            value: `
+            <h1>Contact Us Form Submission</h1>
+            <p><strong>Date:</strong> ${dateFormatted}</p>
+            <p><strong>Name:</strong> ${data.name}</p>
+            <p><strong>Email:</strong> ${data.email}</p>
+            <p><strong>Company Name:</strong> ${data.companyName}</p>
+            <p><strong>Telephone:</strong> ${data.telephone}</p>
+            <p><strong>Message:</strong> ${data.message}</p>
+          `,
+          },
+        ],
+      };
+
+      // Sending of email
+      const response = await fetch("https://api.mailchannels.net/tx/v1/send", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+
+      return new Response("Success", { status: 200 });
     } catch (e) {
-      return new Response(e.message, { status: 400 });
+      console.log(e);
+
+      return new Response("Error", { status: 500 });
     }
   }
 
